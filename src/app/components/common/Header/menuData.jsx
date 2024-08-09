663eef0c58038835ddb231ad40afe02c2edea9a9
Abhishekmathur00 <148318334+Abhishekmathur00@users.noreply.github.com>
2024-08-09 10:04:01 +0530

    initial commit

---
+++ src/app/components/common/Header/menuData.jsx
@@ -0,0 +1,261 @@
+const menuData = [
+  {
+    id: 1,
+    title: "Home",
+    newTab: true,
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Know your doctor",
+    newTab: true,
+    // submenu: [
+    //   {
+    //     id: 21,
+    //     title: "The Corporate",
+    //     newTab: true,
+    //     submenu: [
+    //       {
+    //         id: 211,
+    //         title: "Key MileStones",
+    //         path: "/who-we-are/corporate/key-milestones",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 212,
+    //         title: "Chairman's Desk",
+    //         path: "/who-we-are/corporate/chairman-desk",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 213,
+    //         title: "Overview",
+    //         path: "/who-we-are/corporate/overview",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 214,
+    //         title: "Our Board of Directors",
+    //         path: "/who-we-are/corporate/board-of-directors",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 215,
+    //         title: "Our Leadership and Team",
+    //         path: "/who-we-are/corporate/leadership",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 216,
+    //         title: "CSR",
+    //         path: "/who-we-are/corporate/csr",
+    //         newTab: false,
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     id: 22,
+    //     title: "Global Presence",
+    //     newTab: true,
+    //     submenu: [
+    //       {
+    //         id: 221,
+    //         title: "Global Footprints",
+    //         path: "/who-we-are/global-presence/global-footprints",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 222,
+    //         title: "Global Accreditation",
+    //         path: "/who-we-are/global-presence/global-accreditation",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 223,
+    //         title: "Awards and Recognitions",
+    //         path: "/who-we-are/global-presence/awards-and-recognitions",
+    //         newTab: false,
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     id: 23,
+    //     title: "Corporate Governance",
+    //     newTab: false,
+    //     submenu: [
+    //       {
+    //         id: 231,
+    //         title: "Code of Conduct BMID",
+    //         path: "/pdfs/pdf1.pdf",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 232,
+    //         title: "Code of conduct for Disclosure",
+    //         path: "/pdfs/pdf2.pdf",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 233,
+    //         title: "Code of Conduct",
+    //         path: "/pdfs/pdf3.pdf",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 234,
+    //         title: "Familiarization Programme For Independent Directors",
+    //         path: "/pdfs/pdf4.pdf",
+    //         newTab: false,
+    //       },
+    //     ],
+    //   },
+    // ],
+  },
+  {
+    id: 3,
+    title: "Specialty Services",
+    newTab: true,
+    submenu: [
+      {
+        id: 31,
+        title: "Minimally Invasive Spine Surgery",
+        newTab: true,
+        
+      },
+      {
+        id: 32,
+        title: "Spine Deformity Correction",
+        newTab: true,
+        
+      },
+      {
+        id: 33,
+        title: "Traumatic Conditions of Spine",
+        newTab: false,
+      },
+      {
+        id: 34,
+        title: "Endoscopic Spine Surgery",
+        newTab: false,
+      },
+      {
+        id: 35,
+        title: "Geriatric Spine Surgery",
+        newTab: false,
+      },
+      {
+        id: 36,
+        title: "Artificial Disc Replacement",
+        newTab: false,
+      },
+      {
+        id: 37,
+        title: "Spinal Tumors",
+        newTab: false,
+      },
+      {
+        id: 38,
+        title: "Pain Management",
+        newTab: false,
+      },
+      {
+        id: 39,
+        title: "Rehabilitation Programme",
+        newTab: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Conditions Treated",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "Degenerative Spine Conditions",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Spondylolisthesis",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Spinal Fusion Surgery",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "Spinal Deformities",
+        newTab: false,
+      },
+      {
+        id: 45,
+        title: "Post Infectious Spine Deformities Correction",
+        newTab: false,
+      },
+      {
+        id: 46,
+        title: "Spine Tumors",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Spondylolisthesis",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Pain Procedure",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Spondylolisthesis",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Pediatric Congenital Spine Deformities",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Spondylolisthesis",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Spondylolisthesis",
+        newTab: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Investor",
+    newTab: false,
+    path: "/investor",
+  },
+  {
+    id: 6,
+    title: "News",
+    newTab: false,
+    path: "/news",
+  },
+  {
+    id: 7,
+    title: "Career",
+    newTab: false,
+    path: "/career",
+  },
+  {
+    id: 8,
+    title: "Contact Us",
+    newTab: false,
+    path: "/contact",
+  },
+];
+export default menuData;
+
