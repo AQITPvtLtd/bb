47e2c5fef51d1bba3055ba27090a22b1aafc1fd1
Abhishekmathur00 <148318334+Abhishekmathur00@users.noreply.github.com>
2024-08-21 10:33:12 +0530

    correct media page

--- src/app/media/printMedia/Printmedia.jsx
+++ src/app/media/printMedia/Printmedia.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import 'animate.css';
+import React from "react";
+import "animate.css";
 const Printmedia = () => {
   return (
-    <div className='mt-[200px]'>
-      <h1 className='text-center text-4xl font-semibold animate__animated animate__backInDown'>Coming Soon</h1>
-    </div>
-  )
-}
+    <div className="mt-[200px]">
+      <h1 className="text-center text-4xl font-semibold animate__animated animate__fadeInUp bg-[#07a496] p-10 text-white">
+        Print Media
+      </h1>
 
-export default Printmedia
+      <h1 className="text-center text-4xl py-7 font-semibold animate__animated animate__backInDown">
+        Coming Soon
+      </h1>
+    </div>
+  );
+};
+
+export default Printmedia;
