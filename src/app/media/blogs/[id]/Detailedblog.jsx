4a0f170fcbb0c94526eb63889844886a40fbd581
Abhishekmathur00 <148318334+Abhishekmathur00@users.noreply.github.com>
2024-09-24 12:38:23 +0530

    blog slider added

--- src/app/media/blogs/[id]/Detailedblog.jsx
+++ src/app/media/blogs/[id]/Detailedblog.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { getBlog } from "@/services/getBlog";
 import Image from "next/image";
 import { SlCalender } from "react-icons/sl";
+import Sidebar from "../Sidebar";
 
 const DetailedBlog = ({ id }) => {
   const [blogs, setBlogs] = useState([]);
@@ -23,29 +24,38 @@ const DetailedBlog = ({ id }) => {
 
   return (
     <div className="mt-[100px] px-4 sm:px-6 lg:px-8">
-      <h1 className="text-4xl font-bold text-center mb-12">{obj?.heading}</h1>
+      <div className="lg:grid grid-cols-12 gap-10 justify-between">
+        <div className="col-start-1 col-span-9">
+          <h1 className="text-4xl font-bold text-center mb-[60px]">
+            {obj?.heading}
+          </h1>
 
-      <div className="flex justify-center mb-8">
-        <Image
-          src={`/Blog/${obj?.img}`}
-          alt={obj?.heading}
-          className="w-full h-auto object-cover rounded-lg shadow-lg"
-          width={1000}
-          height={500}
-        />
-      </div>
+          <div className="flex justify-center mb-8">
+            <Image
+              src={`/Blog/${obj?.img}`}
+              alt={obj?.heading}
+              className="w-full h-auto object-cover rounded-lg shadow-lg"
+              width={1000}
+              height={500}
+            />
+          </div>
 
-      <p className="text-gray-500 text-sm mb-4 flex gap-2 items-center">
-        {" "}
-        <SlCalender />
-        {obj?.date}
-      </p>
+          <p className="text-gray-500 text-sm mb-4 flex gap-2 items-center">
+            {" "}
+            <SlCalender />
+            {obj?.date}
+          </p>
 
-      <div className="prose prose-lg mx-auto">
-        <p
-          className="text-gray-700"
-          dangerouslySetInnerHTML={{ __html: obj?.content }}
-        ></p>
+          <div className="prose prose-lg mx-auto">
+            <p
+              className="text-gray-700"
+              dangerouslySetInnerHTML={{ __html: obj?.content }}
+            ></p>
+          </div>
+        </div>
+        <div className="col-span-3 mb-6">
+          <Sidebar />
+        </div>
       </div>
     </div>
   );
