ca68352c727020188b0aa7fe89b8535e6e4906c8
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2024-12-28 17:03:28 +0530

    content added

--- src/app/conditions_treated/spondylolisthesis/Spondylolisthesis.jsx
+++ src/app/conditions_treated/spondylolisthesis/Spondylolisthesis.jsx
@@ -1,17 +1,275 @@
 import React from "react";
 import "animate.css";
-// import AppointmentForm from "@/app/components/homepage/AppoitmentForm";
+import Image from "next/image";
 const Spondylolisthesis = () => {
   return (
     <div className="">
-      <h1 className="text-center text-4xl font-semibold animate__animated animate__fadeInUp bg-[#07a496] p-10 text-white">
-        Spondylolisthesis
-      </h1>
+      <div className="space-y-8">
+        <div className="relative">
+          <Image
+            src="/special_services/geriatric.jpg"
+            width={10000}
+            height={10000}
+            className="w-full h-[250px] object-cover"
+            alt="Service Image"
+          />
+          <div className="w-1/2 text-white pl-16 justify-center lg:text-5xl text-4xl lg:ml-0 ml-5 font-bold my-4 absolute inset-0 z-20 flex items-center">
+            Spondylolisthesis
+          </div>
+        </div>
+      </div>
 
-      <h1 className="text-center text-4xl py-7 font-semibold animate__animated animate__backInDown">
-        Coming Soon
-      </h1>
-      {/* <AppointmentForm /> */}
+      <div className="bg-gray-100 text-gray-800 lg:p-8 p-3">
+        <div className="max-w-5xl mx-auto bg-white lg:p-6 p-4 rounded-lg shadow-lg">
+          <h1 className="text-3xl font-bold text-primary mb-6">
+            Spondylolisthesis
+          </h1>
+
+          <p className="mb-4">
+            Spondylolisthesis is a spinal condition in which one vertebra slips
+            forward over the vertebra below it. This misalignment can cause
+            pain, nerve compression, and difficulty in movement, significantly
+            affecting a person’s quality of life. The condition can occur in any
+            part of the spine but is most common in the lower back (lumbar
+            spine).
+          </p>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Types of Spondylolistheses
+          </h2>
+          <ol className="list-decimal list-inside space-y-4 mb-8">
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Congenital Spondylolisthesis
+              </li>
+              <ul className="list-disc list-inside space-y-2 mb-8 pl-4 mt-2">
+                <li> Caused by a defect in the spine present at birth.</li>
+                <li>
+                  Often involves improper bone development leading to structural
+                  weakness.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Isthmic Spondylolisthesis
+              </li>
+              <ul className="list-disc list-inside space-y-2 pl-4 mt-2">
+                <li>
+                  Occurs due to a stress fracture or defect in the pars
+                  interarticularis (a part of the vertebra).
+                </li>
+                <li>
+                  Commonly affects athletes involved in repetitive spinal
+                  movements, like gymnasts or football players.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Degenerative Spondylolisthesis
+              </li>
+              <ul className="list-disc list-inside space-y-2 pl-4 mt-2">
+                <li>
+                  Caused by age-related wear and tear on spinal joints and
+                  discs.
+                </li>
+                <li>
+                  Typically occurs in older adults and is associated with
+                  conditions like arthritis.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                4. Traumatic Spondylolisthesis
+              </li>
+              <ul className="list-disc list-inside space-y-2 pl-4 mt-2">
+                <li>
+                  Results from an acute injury that causes a fracture or
+                  dislocation of the vertebra.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Pathological Spondylolisthesis
+              </li>
+              <ul className="list-disc list-inside space-y-2 pl-4 mt-2">
+                <li>
+                  Caused by underlying conditions such as tumors, infections, or
+                  diseases that weaken spinal bones.
+                </li>
+              </ul>
+            </div>
+          </ol>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Causes and Risk Factors
+          </h2>
+          <ul className="list-disc list-inside space-y-4 mb-8">
+            <li>
+              <span className="font-semibold"> Repetitive Stress: </span>{" "}
+              Activities that involve frequent back bending or twisting.
+            </li>
+            <li>
+              <span className="font-semibold"> Genetics: </span> A family
+              history of spinal conditions can increase susceptibility.
+            </li>
+
+            <li>
+              <span className="font-semibold"> Aging: </span>
+              Degeneration of spinal components over time.
+            </li>
+
+            <li>
+              <span className="font-semibold"> Injury or Trauma: </span>
+              Sudden force to the spine from accidents or falls.
+            </li>
+
+            <li>
+              <span className="font-semibold">Weak or Thin Bones:</span>
+              Conditions like osteoporosis can contribute to vertebral slippage.
+            </li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">Symptoms</h2>
+
+          <ul className="list-disc list-inside space-y-4 mb-8">
+            <li>Lower back pain, often exacerbated by standing or walking.</li>
+            <li>Stiffness in the back and hamstrings.</li>
+
+            <li>
+              Tingling, numbness, or weakness in the legs due to nerve
+              compression.
+            </li>
+
+            <li>Reduced range of motion in the spine.</li>
+
+            <li>Difficulty walking or maintaining posture.</li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">Diagnosis</h2>
+          <p className="mb-4">Diagnosing spondylolisthesis involves:</p>
+
+          <ol className="list-decimal list-inside space-y-4 mb-8">
+            <li>
+              <span className="font-semibold"> Physical Examination: </span>
+              Evaluating posture, range of motion, and areas of pain.
+            </li>
+            <li>
+              <span className="font-semibold">Imaging Studies:</span> X-rays to
+              detect vertebral misalignment, and MRI or CT scans for a detailed
+              view of nerve involvement.
+            </li>
+
+            <li>
+              <span className="font-semibold"> Neurological Tests: </span>
+              Assessing reflexes, muscle strength, and sensory function.
+            </li>
+          </ol>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Treatment Options
+          </h2>
+          <ol className="list-decimal list-inside space-y-4 mb-8">
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Non-Surgical Treatments
+              </li>
+              <ul className="list-disc list-inside space-y-2 mb-8 pl-4 mt-2">
+                <li>
+                  <span className="font-semibold">
+                    Rest and Activity Modification:
+                  </span>
+                  Avoiding activities that exacerbate symptoms.
+                </li>
+                <li>
+                  <span className="font-semibold"> Physical Therapy:</span>
+                  Strengthening the core and back muscles to stabilize the
+                  spine.
+                </li>
+
+                <li>
+                  <span className="font-semibold"> Medications:</span>
+                  Over-the-counter pain relievers and anti-inflammatory drugs.
+                </li>
+
+                <li>
+                  <span className="font-semibold"> Bracing:</span>
+                  Wearing a back brace to provide support and limit motion.
+                </li>
+
+                <li>
+                  <span className="font-semibold"> Injections:</span>
+                  Epidural steroid injections to reduce inflammation and pain.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Surgical Treatments
+              </li>
+              <ul className="list-disc list-inside space-y-2 pl-4 mt-2">
+                <li>
+                  <span className="font-semibold"> Spinal Fusion: </span>
+                  Stabilizing the spine by fusing the affected vertebrae.
+                </li>
+
+                <li>
+                  <span className="font-semibold">Decompression Surgery: </span>
+                  Removing bone or tissue pressing on nerves.
+                </li>
+
+                <li>
+                  Surgery is typically recommended for severe cases where
+                  conservative treatments fail or if nerve damage progresses.
+                </li>
+              </ul>
+            </div>
+          </ol>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">Prevention</h2>
+
+          <ul className="list-disc list-inside space-y-4 mb-8">
+            <li>
+              Maintain a strong and flexible core through regular exercise.
+            </li>
+            <li>
+              Use proper techniques when lifting heavy objects to avoid spinal
+              stress.
+            </li>
+
+            <li>
+              Avoid repetitive back bending and twisting activities without
+              proper conditioning.
+            </li>
+            <li>
+              Seek early treatment for spinal injuries to prevent progression.
+            </li>
+            <li> Practice good posture in daily activities.</li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Overall Summary
+          </h2>
+          <p>
+            Spondylolisthesis can cause significant discomfort and mobility
+            issues if left untreated. With proper diagnosis and a tailored
+            treatment plan, individuals can manage symptoms effectively and
+            regain their quality of life. Whether through conservative care or
+            surgical intervention, addressing the condition promptly is key to
+            preventing further complications. If you suspect spondylolisthesis,
+            consult a spine specialist to explore the best treatment options for
+            your needs.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
