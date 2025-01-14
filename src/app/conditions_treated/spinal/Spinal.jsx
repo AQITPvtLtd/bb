442a399eb99b425191ad72105908e7a6e2a43f90
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2025-01-14 18:09:22 +0530

    add content

--- src/app/conditions_treated/spinal/Spinal.jsx
+++ src/app/conditions_treated/spinal/Spinal.jsx
@@ -1,17 +1,344 @@
 import React from "react";
 import "animate.css";
+import Image from "next/image";
 // import AppointmentForm from "@/app/components/homepage/AppoitmentForm";
 const Spinal = () => {
   return (
     <div className="">
-      <h1 className="text-center text-4xl font-semibold animate__animated animate__fadeInUp bg-[#07a496] p-10 text-white">
-        Spinal Fusion Surgery
-      </h1>
+      <div className="space-y-8">
+        <div className="relative">
+          <Image
+            src="/special_services/geriatric.jpg"
+            width={10000}
+            height={10000}
+            className="w-full h-[250px] object-cover"
+            alt="Service Image"
+          />
+          <div className="w-1/2 text-white pl-16 justify-center lg:text-5xl text-4xl lg:ml-0 ml-5 font-bold my-4 absolute inset-0 z-20 flex items-center">
+            Spinal Fusion Surgery
+          </div>
+        </div>
+      </div>
 
-      <h1 className="text-center text-4xl py-7 font-semibold animate__animated animate__backInDown">
-        Coming Soon
-      </h1>
-      {/* <AppointmentForm /> */}
+      <div className="bg-gray-100 text-gray-800 lg:p-8 p-3">
+        <div className="max-w-5xl mx-auto bg-white lg:p-6 p-4 rounded-lg shadow-lg">
+          <h1 className="text-3xl font-bold text-primary mb-6">
+            Spinal Fusion Surgery
+          </h1>
+
+          <p className="mb-4">
+            Spinal fusion surgery is a procedure that joins two or more
+            vertebrae in the spine, effectively eliminating motion between them.
+            The goal of spinal fusion is to stabilize the spine, reduce pain,
+            and correct deformities or misalignments. This surgery is typically
+            recommended for patients with conditions like spondylolisthesis,
+            degenerative disc disease, spinal fractures, or scoliosis when
+            conservative treatments have not provided relief.
+          </p>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Types of Spinal Fusion Surgery
+          </h2>
+          <ol className="list-decimal list-outside px-4 space-y-4 mb-8">
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Anterior Cervical Discectomy and Fusion (ACDF)
+              </li>
+              <ul className="list-disc list-outside px-4 space-y-2 mb-8 pl-4 mt-2">
+                <li>
+                  Performed on the neck (cervical spine) to remove a damaged
+                  disc and fuse the adjacent vertebrae.
+                </li>
+                <li>
+                  Commonly used for treating herniated discs or nerve
+                  compression in the cervical spine.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Posterior Lumbar Interbody Fusion (PLIF)
+              </li>
+              <ul className="list-disc list-outside px-4 space-y-2 pl-4 mt-2">
+                <li>
+                  A fusion performed through the back (posterior approach) for
+                  treating lower back (lumbar spine) conditions like
+                  degenerative disc disease or spondylolisthesis.
+                </li>
+                <li>
+                  Involves removal of a damaged disc, followed by insertion of a
+                  bone graft to promote fusion.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Transforaminal Lumbar Interbody Fusion (TLIF)
+              </li>
+              <ul className="list-disc list-outside px-4 space-y-2 pl-4 mt-2">
+                <li>
+                  A similar approach to PLIF but performed from the side to
+                  minimize damage to muscles and tissue.
+                </li>
+                <li>
+                  Often used for lumbar spine issues like degenerative disc
+                  disease or spinal instability.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Lateral Lumbar Interbody Fusion (LLIF)
+              </li>
+              <ul className="list-disc list-outside px-4 space-y-2 pl-4 mt-2">
+                <li>
+                  Involves accessing the spine through the side (lateral
+                  approach) to treat issues in the lower back.
+                </li>
+                <li>
+                  Typically used for conditions like scoliosis or severe disc
+                  degeneration.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Minimally Invasive Spinal Fusion
+              </li>
+              <ul className="list-disc list-outside px-4 space-y-2 pl-4 mt-2">
+                <li>
+                  A less invasive approach using smaller incisions and
+                  specialized instruments to reduce tissue damage and promote
+                  faster recovery.
+                </li>
+                <li>
+                  Used for treating various spinal conditions while minimizing
+                  recovery time and risks.
+                </li>
+              </ul>
+            </div>
+          </ol>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Causes and Indications for Spinal Fusion Surgery
+          </h2>
+          <p className="mb-2">
+            Spinal fusion surgery is considered for the following conditions:
+          </p>
+          <ul className="list-disc list-outside px-4 space-y-4 mb-8">
+            <li>
+              <span className="font-semibold">Degenerative Disc Disease: </span>{" "}
+              When the discs in the spine deteriorate over time, causing pain
+              and instability.
+            </li>
+            <li>
+              <span className="font-semibold">Spondylolisthesis: </span> When a
+              vertebra slips out of place and causes nerve compression.
+            </li>
+
+            <li>
+              <span className="font-semibold">Spinal Fractures: </span>
+              Fractures resulting from trauma or conditions like osteoporosis.
+            </li>
+
+            <li>
+              <span className="font-semibold"> Scoliosis: </span>
+              Abnormal curvature of the spine that requires correction.
+            </li>
+
+            <li>
+              <span className="font-semibold">
+                Spinal Tumors or Infections:
+              </span>{" "}
+              Removing and stabilizing affected vertebrae to prevent further
+              complications.
+            </li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Symptoms Indicating the Need for Spinal Fusion Surgery
+          </h2>
+
+          <ul className="list-disc list-outside px-4 space-y-4 mb-8">
+            <li>
+              Chronic back or neck pain that has not improved with conservative
+              treatments.
+            </li>
+            <li>
+              Severe spinal instability or deformity that affects daily
+              activities.
+            </li>
+
+            <li>
+              Nerve compression symptoms, such as numbness, weakness, or
+              tingling in the legs or arms.
+            </li>
+            <li>
+              Loss of spinal mobility or difficulty with posture and movement.
+            </li>
+            <li>
+              Conditions that affect the spine’s structural integrity, such as
+              fractures or degenerative disc disease.
+            </li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">Diagnosis</h2>
+          <p className="mb-2">
+            The diagnosis for spinal fusion surgery involves several steps:
+          </p>
+          <ol className="list-decimal list-outside px-4 space-y-4 mb-8 ">
+            <li>
+              <span className="font-semibold">Physical Examination:</span>{" "}
+              Assessing the patient’s posture, range of motion, and pain points.
+            </li>
+            <li>
+              <span className="font-semibold">Imaging Studies:</span> X-rays,
+              MRI, or CT scans are used to evaluate the condition of the spine
+              and confirm the need for fusion.
+            </li>
+
+            <li>
+              <span className="font-semibold"> Neurological Tests: </span> To
+              assess nerve function, reflexes, and muscle strength.
+            </li>
+          </ol>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Treatment Options
+          </h2>
+          <ol className="list-decimal list-outside px-4 space-y-4 mb-8">
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Non-Surgical Treatments
+              </li>
+              <ul className="list-disc list-outside px-4 space-y-2 mb-8 pl-4 mt-2">
+                <li>Rest and activity modification.</li>
+                <li>Physical therapy to strengthen supporting muscles.</li>
+                <li>Medications to manage pain and inflammation.</li>
+                <li>
+                  Epidural steroid injections to reduce pain and swelling.
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li className="text-blue-500 font-semibold text-lg">
+                Surgical Treatments
+              </li>
+              <ul className="list-disc list-outside px-4 space-y-2 pl-4 mt-2">
+                <li>
+                  <span className="font-semibold">Spinal Fusion Surgery: </span>
+                  The primary surgical treatment to stabilize the spine.
+                </li>
+
+                <li>
+                  <span className="font-semibold">Decompression Surgery: </span>{" "}
+                  May be combined with fusion to remove pressure on nerves.
+                </li>
+              </ul>
+            </div>
+          </ol>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Procedure Overview
+          </h2>
+
+          <ul className="list-disc list-outside px-4 space-y-4 mb-8">
+            <li>
+              <span className="font-semibold">Bone Graft: </span> A bone graft
+              is inserted between the vertebrae to promote healing and fusion.
+              The graft can be taken from the patient (autograft), a donor
+              (allograft), or synthetic materials.
+            </li>
+            <li>
+              <span className="font-semibold">Surgical Approaches: </span>
+              Depending on the location of the fusion, the surgery may be
+              performed through the back, front, or side of the body.
+            </li>
+            <li>
+              <span className="font-semibold"> Stabilization: </span>
+              Metal implants such as screws, rods, or cages are used to
+              stabilize the spine during the healing process.
+            </li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Recovery and Rehabilitation
+          </h2>
+
+          <ul className="list-disc list-outside px-4 space-y-4 mb-8">
+            <li>
+              <span className="font-semibold">Hospital Stay: </span> Most
+              patients stay in the hospital for 2-4 days, depending on the
+              complexity of the surgery.
+            </li>
+            <li>
+              <span className="font-semibold">Physical Therapy: </span>
+              <span>
+                Patients are encouraged to start physical therapy after the
+                initial recovery period to regain strength and mobility.
+              </span>
+            </li>
+            <li>
+              <span className="font-semibold">Pain Management: </span>
+              Pain relief is provided with medications and possible nerve block
+              injections.
+            </li>
+            <li>
+              <span className="font-semibold"> Activity Restrictions: </span>
+              Avoiding bending, lifting, or twisting motions during the recovery
+              period.
+            </li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Prevention and Lifestyle Adjustments
+          </h2>
+
+          <ul className="list-disc list-outside px-4 space-y-4 mb-8">
+            <li>
+              <span className="font-semibold">Post-Surgical Exercise: </span>
+              Once cleared by a healthcare provider, strengthening exercises can
+              help support spinal health.
+            </li>
+            <li>
+              <span className="font-semibold">Ergonomics: </span>
+              Proper posture and ergonomic adjustments at work and home can
+              reduce strain on the spine.
+            </li>
+            <li>
+              <span className="font-semibold">Weight Management: </span>
+              Maintaining a healthy weight reduces pressure on the spine.
+            </li>
+            <li>
+              <span className="font-semibold">Avoid Heavy Lifting: </span>
+              Avoiding improper lifting techniques that strain the back.
+            </li>
+            <li>
+              <span className="font-semibold">Regular Follow-Ups: </span>
+              Keeping up with regular check-ups to monitor recovery and prevent
+              complications.
+            </li>
+          </ul>
+
+          <h2 className="text-2xl font-bold text-primary mb-4">
+            Overall Summary
+          </h2>
+          <p>
+            Spinal fusion surgery is an effective treatment for stabilizing the
+            spine and alleviating pain in patients with various spinal
+            conditions. While the procedure offers significant relief for many,
+            it is essential to follow proper rehabilitation guidelines for a
+            successful recovery. If you are considering spinal fusion surgery,
+            consult with a spine specialist to determine if it’s the best option
+            for your condition and to discuss the potential benefits and risks.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
