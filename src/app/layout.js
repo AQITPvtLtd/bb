48f9942e01536424c44bb090af8cc8aceaf87b25
Abhishekmathur00 <abhishekmathur7575@gmail.com>
2025-01-29 11:26:31 +0530

    add gtag link

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/common/Header/Header";
 import Footer from "@/components/common/Footer/Footer";
 // import { FaYoutube } from "react-icons/fa"; // Import the YouTube icon from react-icons
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-32BHE7GKMM"></Script>
+        <Script>
+          {`  window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-32BHE7GKMM');`}
+
+        </Script>
+      </head>
       <body className={inter.className}>
         <Header />
 
